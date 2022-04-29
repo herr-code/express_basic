d4bcdd881bedc6034b7a353e2691c54bb0f1dc47
herr-code <herrwebj@gmail.com>
2022-04-29 18:38:48 -0500

    Agrega nueva ruta para regresar un objeto

--- app.js
+++ app.js
@@ -7,13 +7,20 @@ const port = 3000
 
 //path inicial, responderá a la URL localhost:3000
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send("Hello world")
 }); 
 
 //Respondiendo texto
 //URL => localhost:3000/launchx
 app.get('/launchx', (req, res) => {
-  res.send('Bienvenidos a launchx')
+  res.send("Bienvenidos a launchx")
+}); 
+
+//Regresando un objeto
+//URL => localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+  const explorer = { name: "Explorer", msg: "Hello"}
+  res.send(explorer)
 }); 
 
 //Con esto inicializamos esta app
